75979257104ff501c9950c40ced1859742afba3f
suruomo <www.suruomo@hotmail.com>
2019-09-01 21:31:56 +0800

    删除有序数组重复值

---
+++ src/RemoveDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+/**
+ * 问题：Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * 思路：hashset存储不重复集合
+ */
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int[] a={0,0,1,1,1,2,2,3,4,4,5,5};
+        remove(a);
+    }
+
+    private static void remove(int[] a) {
+        HashSet<Integer> hashSet=new HashSet();
+        for(int i:a){
+            hashSet.add(i);
+        }
+        System.out.println(hashSet);
+    }
+}
