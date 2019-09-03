13d27903b1eb902110a82370daffe99316fed6f3
suruomo <www.suruomo@hotmail.com>
2019-09-03 21:45:12 +0800

    最大子串和

---
+++ src/MaxSubArray.java
@@ -0,0 +1,32 @@
+/**
+ * @author 苏若墨
+ * 问题：给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 思路：动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+ * 如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
+ * 如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
+ * 每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
+ * 时间复杂度：O(n)O(n)
+ *
+ */
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+        int ans=0;
+        ans=findMaxSum(nums);
+        System.out.println("最大子串和是："+ans);
+    }
+
+    private static int findMaxSum(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
