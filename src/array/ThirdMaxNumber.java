c21fdb0afe8c8480fad3509c14eaf6edf81e3447
suruomo <www.suruomo@hotmail.com>
2019-09-07 20:46:18 +0800

    找第三大数字

---
+++ src/array/ThirdMaxNumber.java
@@ -0,0 +1,32 @@
+package array;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ThirdMaxNumber {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 2};
+        System.out.println(thirdMaxNumber(nums));
+    }
+
+    private static int thirdMaxNumber(int[] nums) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        for(int num:nums){
+            hashSet.add(num);
+        }
+        int[] index=new int[hashSet.size()];
+        int i=0;
+
+        for(int num:hashSet){
+            index[i++]=num;
+        }
+        Arrays.sort(index);
+        if(index.length<3){
+            return index[index.length-1];
+        }
+        else{
+            return index[2];
+        }
+    }
+}
