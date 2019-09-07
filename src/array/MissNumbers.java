4bf294a720069fa652d68e62187b940eb5913def
suruomo <www.suruomo@hotmail.com>
2019-09-07 21:22:14 +0800

    统计1出现次数

--- src/array/MissNumbers.java
+++ src/array/MissNumbers.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+/**
+ * @author 苏若墨
+ * 问题：给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * 思路：哈希法
+ */
 public class MissNumbers {
     public static void main(String[] args) {
         int[]nums={3,1,0};
