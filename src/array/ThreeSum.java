a3b082a2c7456a1a109421bbc6fed635551ab1c7
suruomo <www.suruomo@hotmail.com>
2019-10-09 13:14:53 +0800

    三数之和

--- src/array/ThreeSum.java
+++ src/array/ThreeSum.java
@@ -15,6 +15,56 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums={-1, 0, 1, 2, -1, -4};
+        threeSum1(nums);
+        System.out.println(threeSum2(nums));
+    }
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        //若数组为空或者数组长度小于3，返回空
+        if(nums == null || len < 3) {
+            return null;
+        }
+        // 数组排序
+        Arrays.sort(nums);
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) {
+                // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+                break;
+            }
+            if(i > 0 && nums[i] == nums[i-1]) {
+                // 去重
+                continue;
+            }
+            //设置两个左右指针向中间移动
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) {
+                        L++; // 去重
+                    }
+                    while (L<R && nums[R] == nums[R-1]) {
+                        R--; // 去重
+                    }
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) {
+                    L++;
+                }
+                else if (sum > 0) {
+                    R--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    private static void threeSum1(int[] nums) {
         //对数组进行排序
         Arrays.sort(nums);
         List<List<Integer>> ls = new ArrayList<>();
