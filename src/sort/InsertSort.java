a966f00f84e321eedde30dcc5da4643fb7341cc6
suruomo <www.suruomo@hotmail.com>
2019-10-28 11:36:54 +0800

    插入排序

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -7,6 +7,22 @@ package sort;
  */
 public class InsertSort {
     public static void main(String[] args) {
+        int[] nums = {3, 2, 7, 4, 8, 9, 0, 1, 6, 5};
+        insertSort(nums);
+        System.out.println("插入排序后数组是：");
+        for (int num : nums) {
+            System.out.print(num + ",");
+        }
+    }
 
+    private static void insertSort(int[] nums) {
+        int j=0;
+        for(int i=1;i<nums.length;i++){
+            int temp=nums[i];
+            for(j=i;j>0&&nums[j-1]>temp;j--){
+                nums[j]=nums[j-1];
+            }
+            nums[j]=temp;
+        }
     }
 }
