d8e81ca8978695304f959507d6c3bf6e42e944f8
suruomo <www.suruomo@hotmail.com>
2019-11-12 23:26:07 +0800

    图的DFS递归和非递归遍历

---
+++ src/graph/traversing/Graph.java
@@ -0,0 +1,113 @@
+package graph.traversing;
+
+import java.util.Stack;
+
+/**
+ * 图的DFS和BFS实现
+ *
+ * @author 苏若墨
+ */
+public class Graph {
+    /**
+     * 顶点集合
+     */
+    private char[] vertex;
+    /**
+     * 邻接矩阵
+     */
+    private int[][] matrix;
+    /**
+     * 最大值，表示两个顶点之间不连通
+     */
+    private static final int INF = Integer.MAX_VALUE;
+
+    /**
+     * 用提供的顶点和邻接矩阵创建图
+     *
+     * @param vertex
+     * @param matrix
+     */
+    public Graph(char[] vertex, int[][] matrix) {
+        this.vertex = vertex;
+        this.matrix = matrix;
+    }
+
+    /**
+     * 返回顶点vertex的第一个邻接顶点的索引，失败返回-1
+     *
+     * @param vertex
+     * @return
+     */
+    public int firstVertex(int vertex) {
+        if (vertex < 0 || vertex > (this.vertex.length - 1)) {
+            return -1;
+        }
+        for (int i = 0; i < this.vertex.length; i++) {
+            if (this.matrix[vertex][i] != 0 && this.matrix[vertex][i] != INF) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 返回顶点vertex相对于它的一个邻接顶点index的下一个邻接顶点索引
+     *
+     * @param vertex
+     * @return
+     */
+    public int nextVertex(int vertex, int index) {
+        if (vertex < 0 || vertex > (this.vertex.length - 1) || index < 0 || index > (this.vertex.length - 1)) {
+            return -1;
+        }
+        for (int i = index + 1; i < this.vertex.length; i++) {
+            if (this.matrix[vertex][i] != 0 && this.matrix[vertex][i] != INF) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * DFS:深度优先遍历递归版
+     *
+     * @param i
+     * @param visited
+     */
+    public void DFS(int i, boolean[] visited) {
+        //标记当前顶点以访问
+        visited[i] = true;
+        System.out.print(vertex[i] + "-->");
+        //遍历该顶点的邻接顶点，若是没有访问过，继续往下遍历
+        for (int index = firstVertex(i); index >= 0; index = nextVertex(i, index)) {
+            if (!visited[index]) {
+                DFS(index, visited);
+            }
+        }
+    }
+
+    /**
+     * DFS:深度优先遍历非递归版
+     */
+    public void DFS() {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[this.vertex.length];
+        //开始顶点入栈，标记已访问
+        stack.push(0);
+        visited[0] = true;
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            System.out.print(vertex[current] + "-->");
+            for (int i = 0; i < this.vertex.length; i++) {
+                if (!visited[i] && matrix[current][i] != INF && matrix[current][i] != 0) {
+                    visited[i] = true;
+                    stack.push(i);
+                    break;
+                }
+            }
+
+        }
+    }
+
+}
+
---
+++ src/graph/traversing/Test.java
@@ -0,0 +1,28 @@
+package graph.traversing;
+
+/**
+ * @author 苏若墨
+ */
+public class Test {
+    public static void main(String[] args) {
+        final int INF = Integer.MAX_VALUE;
+        char[] vertex = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        int[][] matrix = {
+                /*A*//*B*//*C*//*D*//*E*//*F*//*G*/
+                /*A*/ {   0,  12, INF, INF, INF,  16,  14},
+                /*B*/ {  12,   0,  10, INF, INF,   7, INF},
+                /*C*/ { INF,  10,   0,   3,   5,   6, INF},
+                /*D*/ { INF, INF,   3,   0,   4, INF, INF},
+                /*E*/ { INF, INF,   5,   4,   0,   2,   8},
+                /*F*/ {  16,   7,   6, INF,   2,   0,   9},
+                /*G*/ {  14, INF, INF, INF,   8,   9,   0}};
+        boolean[] visited=new boolean[vertex.length];
+        Graph graph=new Graph(vertex,matrix);
+        System.out.println("DFS递归遍历：");
+        graph.DFS(0,visited);
+        System.out.println();
+        System.out.println("DFS非递归遍历：");
+        graph.DFS();
+
+    }
+}
