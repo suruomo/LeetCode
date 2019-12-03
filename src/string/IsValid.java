9c1793573683daaf78eeb8eb8e0094ecb3a782de
suruomo <www.suruomo@hotmail.com>
2019-12-03 20:44:20 +0800

    括号匹配

---
+++ src/string/IsValid.java
@@ -0,0 +1,60 @@
+package string;
+
+import java.util.Stack;
+
+/**
+ * 问题：给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串
+ *
+ * @author 苏若墨
+ */
+public class IsValid {
+    public static void main(String[] args) {
+        String s="";
+        System.out.println(isValid(s));
+    }
+
+    private static boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        char[] chars=s.toCharArray();
+        Stack<Character> stack=new Stack<>();
+        int index=0;
+        stack.push(chars[index++]);
+        while(index<chars.length){
+            char c=chars[index++];
+            if(c=='['||c=='{'||c=='('){
+                stack.push(c);
+                continue;
+            }
+            if(c==')'&&stack.peek()=='('){
+                stack.pop();
+                continue;
+            }
+            if(c==']'&&stack.peek()=='['){
+                stack.pop();
+                continue;
+            }
+            if(c=='}'&&stack.peek()=='{'){
+                stack.pop();
+                continue;
+            }
+            return false;
+        }
+        if(!stack.isEmpty()) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
