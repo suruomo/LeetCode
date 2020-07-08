f335431c8cbf689cdc10ffe36681b07a6b38687b
suruomo <www.suruomo@hotmail.com>
2020-07-08 10:29:23 +0800

    修改01背包

--- src/main/java/dynamicplanning/BeiBao.java
+++ src/main/java/dynamicplanning/BeiBao.java
@@ -1,5 +1,7 @@
 package dynamicplanning;
 
+import java.util.Arrays;
+
 /**
  * 动态规划;0-1背包
  *
@@ -23,22 +25,44 @@ public class BeiBao {
 
         }
         //i：只拿前i件物品（这里的i因为取了0，所以对应到weight和value里面都是i-1号位置）
-        //j：假设能取的总重量为j
+        //j：假设当前还能放的背包重量为j
         //n是物品件数
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= maxweight; j++) {
-                //当前最大价值等于放上一件的最大价值
+                //不放当前，当前最大价值等于放上一件物品的最大价值
                 maxvalue[i][j] = maxvalue[i - 1][j];
                 //如果当前件的重量小于总重量，可以放进去或者拿出别的东西再放进去
                 if (weight[i - 1] <= j) {
                     //比较（不放这个物品的价值）和
                     //（这个物品的价值 加上 当前能放的总重量减去当前物品重量时取前i-1个物品时的对应重量时候的最高价值）
-                    if (maxvalue[i - 1][j - weight[i - 1]] + value[i - 1] > maxvalue[i - 1][j]) {
-                        maxvalue[i][j] = maxvalue[i - 1][j - weight[i - 1]] + value[i - 1];
-                    }
+                    maxvalue[i][j] = Math.max(maxvalue[i - 1][j - weight[i - 1]] + value[i - 1],maxvalue[i - 1][j]);
                 }
             }
         }
+
+        //找出价值最大时，包内装入了哪些物品？
+        // 下标i对应的物品若被选中，设置值为1
+        int[] item = new int[n + 1];
+        // 将数组item的所有元素初始化为0
+        Arrays.fill(item, 0);
+
+        // 从最优解，倒推回去找
+        int j = maxweight;
+        for (int i = n; i > 0; i--) {
+            // 在最优解中，maxvalue[i][j] > maxvalue[i - 1][j]说明选择了第i个商品
+            if (maxvalue[i][j] > maxvalue[i - 1][j]) {
+                item[i] = 1;
+                j = j - weight[i];
+            }
+        }
+
+        System.out.print("包内物品的编号为：");
+        for (int i = 0; i < n + 1; i++) {
+            if (item[i] == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
         return maxvalue[n][maxweight];
     }
 
