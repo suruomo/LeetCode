4301aa6921282587cc8d010073279577d0aa6440
suruomo <www.suruomo@hotmail.com>
2020-09-01 13:48:07 +0800

    预测赢家

---
+++ src/main/java/dynamicplanning/PredictTheWinner.java
@@ -0,0 +1,23 @@
+package dynamicplanning;
+
+/**
+ * @Author: suruomo
+ * @Date: 2020/9/1 13:38
+ * @Description: 486 预测赢家
+ */
+public class PredictTheWinner {
+    public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        //dp[i][j] 表示当数组剩下的部分为下标 i 到下标 j 时，当前玩家与另一个玩家的分数之差的最大值，注意当前玩家不一定是先手。
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
+    }
+}
