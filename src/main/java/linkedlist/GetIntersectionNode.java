d7e53d9f424346c9d95a30c5d68d38ccdc8ad438
suruomo <www.suruomo@hotmail.com>
2020-09-15 10:57:37 +0800

    相交链表

---
+++ src/main/java/linkedlist/GetIntersectionNode.java
@@ -0,0 +1,40 @@
+package linkedlist;
+
+/**
+ * @author: suruomo
+ * @date: 2020/9/15 10:47
+ * @description: 160. 相交链表
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ */
+public class GetIntersectionNode {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+            next = null;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA.next;
+            pB = pB.next;
+            if (pA == null && pB == null) {
+                return null;
+            }
+            if (pA == null) {
+                pA = headB;
+            }
+            if (pB == null) {
+                pB = headA;
+            }
+        }
+        return pA;
+    }
+}
