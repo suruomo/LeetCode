31c1568e3950db87c3fbdd79255d2b0e1e9ba8f2
suruomo <www.suruomo@hotmail.com>
2020-09-22 14:07:14 +0800

    二叉树的最近公共祖先

---
+++ src/main/java/tree/LowestCommonAncestor.java
@@ -0,0 +1,40 @@
+package tree;
+
+
+/**
+ * @author: suruomo
+ * @date: 2020/9/22 13:52
+ * @description: 236. 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private TreeNode ans;
+
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        //判断
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+}
