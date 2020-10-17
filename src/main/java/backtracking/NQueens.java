97f4430808182cde6c34428f2c56fefc782010fb
suruomo <www.suruomo@hotmail.com>
2020-10-17 15:00:51 +0800

    N皇后

---
+++ src/main/java/backtracking/NQueens.java
@@ -0,0 +1,95 @@
+package backtracking;
+
+import java.util.*;
+
+/**
+ * @author: suruomo
+ * @date: 2020/10/17 14:31
+ * @description: 51. N皇后
+ */
+public class NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        // 结果集合
+        List<List<String>> solutions = new ArrayList<List<String>>();
+        // 记录每行放置的皇后的列下标
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        // 记录每一列上是否有皇后
+        Set<Integer> columns = new HashSet<Integer>();
+        // 记录左上右下斜线是否有皇后
+        Set<Integer> diagonals1 = new HashSet<Integer>();
+        // 记录右上左下斜线是否有皇后
+        Set<Integer> diagonals2 = new HashSet<Integer>();
+        backtrack(solutions, queens, n, 0, columns, diagonals1, diagonals2);
+        return solutions;
+    }
+
+    /**
+     * 基于集合的回溯
+     * @param solutions 结果集
+     * @param queens 记录每行放置的皇后的列下标
+     * @param n N皇后
+     * @param row 当前遍历行号
+     * @param columns 记录每一列上是否有皇后
+     * @param diagonals1 记录左上右下斜线是否有皇后
+     * @param diagonals2 记录右上左下斜线是否有皇后
+     */
+    public void backtrack(List<List<String>> solutions, int[] queens, int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        // 遍历到最后一行，求得一个解
+        if (row == n) {
+            // 转变结果
+            List<String> board = generateBoard(queens, n);
+            // 加入结果集
+            solutions.add(board);
+        } else {
+            // 遍历当前行的每一列
+            for (int i = 0; i < n; i++) {
+                // 判断每个位置相对的三个集合是否包含皇后
+                if (columns.contains(i)) {
+                    continue;
+                }
+                // 该斜线上的每个位置满足：行下标与列下标之差相等
+                // 因此判断集合中是否包含该差值
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                // 该斜线的每个位置满足：行下标与列下标之和相等
+                // 因此判断集合中是否包含该和值
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                // 1. 选择
+                queens[row] = i;
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                // 2.回溯下一行
+                backtrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
+                // 3.取消当前选择
+                queens[row] = -1;
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+        }
+    }
+
+    /**
+     * 生成结果要求棋盘格式
+     * @param queens
+     * @param n
+     * @return
+     */
+    public List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
+    }
+}
