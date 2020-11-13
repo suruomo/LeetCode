3706feb39238773c2091fa1f70ab89d0016bf335
suruomo <www.suruomo@hotmail.com>
2020-11-13 13:14:13 +0800

    奇偶链表

---
+++ src/main/java/linkedlist/OddEvenList.java
@@ -0,0 +1,48 @@
+package linkedlist;
+
+/**
+ * @author: suruomo
+ * @date: 2020/11/13 12:15
+ * @description: 328 奇偶链表
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。
+ * 请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ */
+public class OddEvenList {
+    /**
+     * 链表定义
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        // head为奇数头节点，evenHead为偶数头节点
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+
+    }
+}
