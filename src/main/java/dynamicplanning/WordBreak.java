887882f5e1cdebe58fff4ce47a92863f42eca799
suruomo <www.suruomo@hotmail.com>
2020-11-26 15:35:57 +0800

    单词拆分修改

--- src/main/java/dynamicplanning/WordBreak.java
+++ src/main/java/dynamicplanning/WordBreak.java
@@ -20,11 +20,12 @@ public class WordBreak {
         Set<String> set=new HashSet(wordDict);
         // dp[i] 表示字符串 s 前 i 个字符组成的字符串s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词
         boolean[] dp=new boolean[s.length()+1];
+        dp[0]=true;
         for (int i = 1; i <=s.length(); i++) {
             // 枚举s[0..i−1] 中的分割点 j,看前后两个字符串是否合法
             for (int j = 0; j < i; j++) {
                 if(dp[j]&&set.contains(s.substring(j,i))){
-                    dp[j]=true;
+                    dp[i]=true;
                     break;
                 }
 
