9cc2b06bc256387f7beffca942c7f421622dc968
suruomo <www.suruomo@hotmail.com>
2020-12-02 13:19:10 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- src/main/java/array/SearchRange.java
+++ src/main/java/array/SearchRange.java
@@ -3,49 +3,43 @@ package array;
 /**
  * @author suruomo
  * @date 2020/8/8 15:10
- * @description: 34
+ * @description: 34 在排序数组中查找元素的第一个和最后一个位置
  * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
  *
- * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 你的算法时间复杂度必须是O(log n) 级别。
  *
- * 如果数组中不存在目标值，返回 [-1, -1]。
+ * 如果数组中不存在目标值，返回[-1, -1]。
  *
  */
 public class SearchRange {
-    // returns leftmost (or rightmost) index at which `target` should be
-    // inserted in sorted array `nums` via binary search.
-    private int extremeInsertionIndex(int[] nums, int target, boolean left) {
-        int lo = 0;
-        int hi = nums.length;
-
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-            if (nums[mid] > target || (left && target == nums[mid])) {
-                hi = mid;
-            }
-            else {
-                lo = mid+1;
-            }
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
         }
-
-        return lo;
+        return new int[]{-1, -1};
     }
 
-    public int[] searchRange(int[] nums, int target) {
-        int[] targetRange = {-1, -1};
-        //查找左边界
-        int leftIdx = extremeInsertionIndex(nums, target, true);
-
-        // assert that `leftIdx` is within the array bounds and that `target`
-        // is actually in `nums`.
-        if (leftIdx == nums.length || nums[leftIdx] != target) {
-            return targetRange;
+    /**
+     * 当lower==true时查找左边界，第一个大于等于target
+     * 当lower==false时查找右边界，第一个大于target的下标减一
+     * @param nums
+     * @param target
+     * @param lower
+     * @return
+     */
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-
-        targetRange[0] = leftIdx;
-        //查找右边界
-        targetRange[1] = extremeInsertionIndex(nums, target, false)-1;
-
-        return targetRange;
+        return ans;
     }
 }
