d84ed1766cd652a01f9e5c0f6502ccf9f09c13f5
suruomo <www.suruomo@hotmail.com>
2020-12-16 11:36:07 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

--- src/main/java/offer/LevelOrder.java
+++ src/main/java/offer/LevelOrder.java
@@ -2,12 +2,15 @@ package offer;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
  * @author: suruomo
  * @date: 2020/12/7 10:37
- * @description: 剑指 Offer 32 - I. 从上到下打印二叉树
+ * @description:
+ * 剑指 Offer 32 - I. 从上到下打印二叉树
+ * 剑指 Offer 32 - II. 从上到下打印二叉树 II
  */
 public class LevelOrder {
     public class TreeNode{
@@ -16,6 +19,12 @@ public class LevelOrder {
         TreeNode  right;
         TreeNode(int x) { val = x; }
     }
+
+    /**
+     * 1.按层打印二叉树
+     * @param root
+     * @return
+     */
     public int[] levelOrder(TreeNode root) {
         if (root==null){
             return new int[0];
@@ -39,5 +48,37 @@ public class LevelOrder {
             res[i]=list.get(i);
         }
         return res;
+    }
+
+    /**
+     * 2.按层打印二叉树，每一层打印到一行
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder1(TreeNode root){
+        if (root==null){
+            return new ArrayList<>();
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        List<List<Integer>> lists=new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            // 临时存储一行数据
+            List<Integer> list=new ArrayList<>();
+            // 当前行数据为当前队列中数据
+            int n=queue.size();
+            for (int i = 0; i <n; i++) {
+                TreeNode node=queue.poll();
+                list.add(node.val);
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
     }
 }
