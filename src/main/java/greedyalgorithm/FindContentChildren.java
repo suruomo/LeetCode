61d74ec74b2aeeccd162e3cc0d9b2134426fce35
suruomo <www.suruomo@hotmail.com>
2020-12-25 15:36:53 +0800

    分发饼干

--- src/main/java/greedyalgorithm/FindContentChildren.java
+++ src/main/java/greedyalgorithm/FindContentChildren.java
@@ -1,12 +1,11 @@
 package greedyalgorithm;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
  * 问题：假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
- * 对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；
- * 并且每块饼干 j ，都有一个尺寸 sj 。如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
+ * 对每个孩子 i ，都有一个胃口值g[i] ，这是能让孩子们满足胃口的饼干的最小尺寸；
+ * 并且每块饼干 j ，都有一个尺寸 s[j]。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
  * 你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
  * 注意：
  * 你可以假设胃口值为正。
@@ -36,5 +35,27 @@ public class FindContentChildren {
             j++;
         }
         return i;
+    }
+
+    /**
+     * 排序+贪心
+     * @param g
+     * @param s
+     * @return
+     */
+    private static int findContentChildren1(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int numOfChildren = g.length, numOfCookies = s.length;
+        int count = 0;
+        for (int i = 0, j = 0; i < numOfChildren && j < numOfCookies; i++, j++) {
+            while (j < numOfCookies && g[i] > s[j]) {
+                j++;
+            }
+            if (j < numOfCookies) {
+                count++;
+            }
+        }
+        return count;
     }
 }
