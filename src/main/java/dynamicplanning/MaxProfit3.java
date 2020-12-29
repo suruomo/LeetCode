aa939dd2f3ea31fab764755cdb11a812c3e45d57
suruomo <www.suruomo@hotmail.com>
2020-12-29 14:33:07 +0800

    优化买卖股票

--- src/main/java/dynamicplanning/MaxProfit3.java
+++ src/main/java/dynamicplanning/MaxProfit3.java
@@ -20,7 +20,7 @@ public class MaxProfit3 {
         }
         int n= prices.length;
         // 所以由于一买一卖，交易肯定不会大于n/2
-        k=Math.max(k,n/2);
+        k=Math.min(k,n/2);
         // buy[i][j] 表示对于数组prices[0..i]中的价格而言，进行恰好j笔交易
         // 并且当前手上持有一支股票，这种情况下的最大利润
         int[][] buy=new int[n][k+1];
@@ -41,5 +41,38 @@ public class MaxProfit3 {
         }
         // 最终的答案即为sell[n−1][0..k] 中的最大值。
         return Arrays.stream(sell[n-1]).max().getAsInt();
+    }
+
+    /**
+     * 优化空间
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[i] = sell[i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+                sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell).max().getAsInt();
     }
 }
