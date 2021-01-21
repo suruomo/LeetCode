edf1cd4e1d32674562793ba3e0dc17c69f8a9091
suruomo <www.suruomo@hotmail.com>
2021-01-21 11:14:51 +0800

    求和

---
+++ src/main/java/offer/SumNums.java
@@ -0,0 +1,19 @@
+package offer;
+
+/**
+ * @author: suruomo
+ * @date: 2021/1/21 11:11
+ * @description: 剑指 Offer 64. 求1+2+…+n
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class SumNums {
+
+    public int sumNums(int n) {
+        boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
+        return n;
+    }
+    public int sumNums1(int n) {
+        boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
+        return n;
+    }
+}
