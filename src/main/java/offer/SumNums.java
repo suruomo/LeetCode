880d6e7b74005eeb9496a9fd6559775662685d6f
suruomo <www.suruomo@hotmail.com>
2021-01-21 11:23:16 +0800

    求和修改

--- src/main/java/offer/SumNums.java
+++ src/main/java/offer/SumNums.java
@@ -7,12 +7,7 @@ package offer;
  * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
  */
 public class SumNums {
-
     public int sumNums(int n) {
-        boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
-        return n;
-    }
-    public int sumNums1(int n) {
         boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
         return n;
     }
