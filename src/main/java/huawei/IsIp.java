0dd7bf774b92c84dd83c2c742ac8463ff72b37d8
suruomo <www.suruomo@hotmail.com>
2021-03-22 09:50:34 +0800

    判断IP

--- src/main/java/huawei/IsIp.java
+++ src/main/java/huawei/IsIp.java
@@ -1,34 +1,34 @@
 package huawei;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * @author: suruomo
  * @date: 2021/3/18 16:15
- * @description:
+ * @description: 判断一个地址是否是合法的IPV4地址
  */
 public class IsIp {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        while (sc.hasNext()){
-            String ip=sc.nextLine();
-            if (isValid(ip)){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String ip = sc.nextLine();
+            if (isValid(ip)) {
                 System.out.println("YES");
-            }else {
+            } else {
                 System.out.println("NO");
             }
         }
     }
 
     private static boolean isValid(String ip) {
-        String[] strings=ip.split("\\.");
-        int[] ips=new int[4];
+        String[] strings = ip.split("\\.");
+        int[] ips = new int[4];
         for (int i = 0; i < 4; i++) {
-            ips[i]=Integer.valueOf(strings[i]);
-            if (ips[i]<0||ips[i]>255){
+            ips[i] = Integer.valueOf(strings[i]);
+            if (ips[i] < 0 || ips[i] > 255) {
                 return false;
             }
+
         }
         return true;
     }
