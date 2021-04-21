57c66d99f3f43911e0cf7eb3502643e8c0588b01
suruomo <www.suruomo@hotmail.com>
2021-04-21 13:47:04 +0800

    是否对称

---
+++ src/main/java/interview/IsSymmetric.java
@@ -0,0 +1,32 @@
+package interview;
+
+/**
+ * @author: suruomo
+ * @date: 2021/4/16 15:09
+ * @description:
+ */
+public class IsSymmetric {
+    public class TreeNode{
+        int val=0;
+        TreeNode left=null;
+        TreeNode right=null;
+    }
+    /**
+     *
+     * @param root TreeNode类
+     * @return bool布尔型
+     */
+    public boolean isSymmetric (TreeNode root) {
+        return root==null?true:recur(root.left,root.right);
+    }
+
+    private boolean recur(TreeNode left, TreeNode right) {
+        if (left==null&&right==null){
+            return true;
+        }
+        if (left==null||right==null||left.val!=right.val){
+            return false;
+        }
+        return recur(left.left,right.right)&&recur(left.right,right.left);
+    }
+}
