3ef84e868dd66ac74ca7a0ec5cff48ac641eb7b3
suruomo <www.suruomo@hotmail.com>
2021-05-20 12:04:19 +0800

    二叉树的堂兄弟节点

---
+++ src/main/java/tree/IsCousins.java
@@ -0,0 +1,94 @@
+package tree;
+
+/**
+ * @author: suruomo
+ * @date: 2021/5/20 11:54
+ * @description: 993. 二叉树的堂兄弟节点
+ * 在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+ * <p>
+ * 如果二叉树的两个节点深度相同，但 父节点不同 ，则它们是一对堂兄弟节点。
+ * <p>
+ * 我们给出了具有唯一值的二叉树的根节点 root ，以及树中两个不同节点的值 x 和 y 。
+ * <p>
+ * 只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true 。否则，返回 false。
+ */
+public class IsCousins {
+    /**
+     * 结点定义
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // x 的信息
+    int x;
+    // x的父节点
+    TreeNode xParent;
+    // x的深度
+    int xDepth;
+    boolean xFound = false;
+
+    // y 的信息
+    int y;
+    TreeNode yParent;
+    int yDepth;
+    boolean yFound = false;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        this.x = x;
+        this.y = y;
+        dfs(root, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    /**
+     * 深度优先遍历，保存当前深度和父节点
+     * @param node 当前遍历节点
+     * @param depth 当前深度
+     * @param parent 当前节点的父节点
+     */
+    public void dfs(TreeNode node, int depth, TreeNode parent) {
+        if (node == null) {
+            return;
+        }
+        // 判断是否是x/y节点
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+            xFound = true;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+            yFound = true;
+        }
+
+        // 如果两个节点都找到了，就可以提前退出遍历
+        // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.left, depth + 1, node);
+
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.right, depth + 1, node);
+    }
+}
