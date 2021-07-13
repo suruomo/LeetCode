6bee0fe1ddc1ba160523164081ae19973147c239
susu_temp <www.suruomo@hotmail.com>
2021-07-13 17:01:19 +0800

    update

--- src/main/java/interview/BigNumber.java
+++ src/main/java/interview/BigNumber.java
@@ -8,9 +8,12 @@ import java.util.Arrays;
  * @Description: 给定一个数组由一些非负整数组成，现需要将他们进行排列并拼接，使得最后的结果最大，返回值需要是string类型 否则可能会溢出
  */
 public class BigNumber {
-
-    public String solve (int[] nums) {
-        if (nums[0]==0){
+    public static void main(String[] args) {
+        int[] nums={10,1};
+        System.out.println(solve(nums));
+    }
+    public static String solve (int[] nums) {
+        if (nums.length>0&&nums[0]==0){
             return "0";
         }
         StringBuilder ans = new StringBuilder();
